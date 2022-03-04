eb08882c2e675bc0ca7ae5477bef214661a60734
Eric Wendelin <eric@gradle.com>
2022-03-04 15:00:33 -0700

    Filter data by geo bounds during cleaning

--- src/basic_cleaning/basic_cleaning.py
+++ src/basic_cleaning/basic_cleaning.py
@@ -24,6 +24,10 @@ def go(args):
     logger.info("Filtering data by price range")
     df = df[(df.price >= args.min_price) & (df.price <= args.max_price)]
 
+    logger.info("Filtering data by geographical bounds")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     df.to_csv(args.output_artifact, index=False)
 
     logger.info("Logging artifact")
